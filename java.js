4915e0233e750569998ef512ab62fda63d6ccfee
thuhoai2210 <106927220+thuhoai2210@users.noreply.github.com>
2022-07-11 01:34:26 +0700

    Add files via upload

--- java.js
+++ java.js
@@ -1,20 +1,17 @@
-// mybutton = document.getElementById("headerweb");
-// window.onscroll = function () {
-//     scrollFunction();
-//     console.log(111);
-// }
-// function scrollFunction() {
-//     console.log('document.body.scrollTop', document.body.scrollTop);
-//     console.log('document.documentElement.scrollTop', document.documentElement.scrollTop);
-//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//         headerweb.style.display = "block";
-//     }
-//     else {
-//         headerweb.style.display = "none";
-//     }
-// }
-// function topFunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// }
+mybutton = document.getElementById("headerweb");
+window.onscroll = function () {
+    scrollFunction();
+}
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        headerweb.style.display = "flex";
+    }
+    else {
+        headerweb.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
