7b7cdfcecb4fab95865e112eaa92b63a075efd0e
thuhoai2210 <106927220+thuhoai2210@users.noreply.github.com>
2022-07-10 23:18:14 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+// mybutton = document.getElementById("headerweb");
+// window.onscroll = function () {
+//     scrollFunction();
+//     console.log(111);
+// }
+// function scrollFunction() {
+//     console.log('document.body.scrollTop', document.body.scrollTop);
+//     console.log('document.documentElement.scrollTop', document.documentElement.scrollTop);
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//         headerweb.style.display = "block";
+//     }
+//     else {
+//         headerweb.style.display = "none";
+//     }
+// }
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
